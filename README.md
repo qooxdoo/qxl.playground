9dbc4af307187cf41795e036d95599817749fb7a
Henner Kollmann <henner.kollmann@gmx.de>
2018-12-23 17:03:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Qooxdoo Playground
+
+A playground for application coding.
+This application features a code editor and a live preview of the application.
+
+## Developing Playground
+Clone this repo and compile it:
+
+```
+    $ git clone https://github.com/qooxdoo/qxl.playground
+    $ cd qxl.playground
+    $ qx contrib install
+    $ npm install
+    $ qx serve
+```
+Then open [http://localhost:8080](http://localhost:8080)
+
---
+++ source/class/qxl/playground/theme/Theme.js
@@ -0,0 +1,31 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+/**
+ * Playgrounds default theme, mostly the indigo theme.
+ */
+qx.Theme.define("qxl.playground.theme.Theme",
+{
+  meta :
+  {
+    color : qx.theme.indigo.Color,
+    decoration : qxl.playground.theme.Decoration,
+    font : qxl.playground.theme.Font,
+    appearance : qxl.playground.theme.Appearance,
+    icon : qx.theme.icon.Tango
+  }
+});
